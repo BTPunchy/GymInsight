691199c15a937d3de57439c389e10ddc537fca3f
Bigga_Cbc_2003 <biggasoftware@gmail.com>
2025-11-14 19:52:01 +0700

    purge

--- frontend/src/src/api/client.js
+++
@@ -1,9 +0,0 @@
-const API_BASE = "http://localhost:1234"; // backend port
-
-export async function registerUser(payload) {
-  return fetch(`${API_BASE}/users/register`, {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(payload),
-  }).then(res => res.json());
-}
